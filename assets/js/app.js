87ab0cc4d04a9a25029b9bd4a49fdf8f548f7112
StefAW <albertwelong@gmail.com>
2024-07-11 01:02:19 -0400

    fixed nav bar resize

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,70 +1,49 @@
-// // Nav hamburgerburger selections
-// const burger = document.querySelector("#burger-menu");
-// const ul = document.querySelector("nav ul");
-// const nav = document.querySelector("nav");
 
-// // Scroll to top selection
-// const scrollUp = document.querySelector("#scroll-up");
+// document.addEventListener("DOMContentLoaded", () => {
+//   // Nav hamburger selections
+//   const burger = document.querySelector("#burger-menu");
+//   const ul = document.querySelector("nav ul");
+//   const nav = document.querySelector("nav");
 
-// // Select nav links
-// const navLink = document.querySelectorAll(".nav-link");
+//   // Scroll to top selection
+//   // const scrollUp = document.querySelector("#scroll-up");
 
-// // Hamburger menu function
-// burger.addEventListener("click", () => {
-//   ul.classList.toggle("show");
-// });
+//   // Select nav links
+//   const navLink = document.querySelectorAll(".nav-link");
+//   ul.classList.remove("show");
 
-// // Close hamburger menu when a link is clicked
-// navLink.forEach((link) =>
-//   link.addEventListener("click", () => {
-//     ul.classList.remove("show");
-//   })
-// );
-
-// // scroll to top functionality
-// scrollUp.addEventListener("click", () => {
-//   window.scrollTo({
-//     top: 0,
-//     left: 0,
-//     behavior: "smooth",
+//   // Hamburger menu function
+//   burger.addEventListener("click", () => {
+//     // console.log("Clicked burger menu");
+//     ul.classList.toggle("show");
 //   });
+
+
+
+//   // Close hamburger menu when a link is clicked
+//   // navLink.forEach((link) =>
+//   //   link.addEventListener("click", () => {
+//   //     ul.classList.remove("show");
+//   //   })
+//   // );
+
+//   // Scroll to top functionality
+//   // scrollUp.addEventListener("click", () => {
+//   //   window.scrollTo({
+//   //     top: 0,
+//   //     left: 0,
+//   //     behavior: "smooth",
+//   //   });
+//   // });
 // });
 
-
-document.addEventListener("DOMContentLoaded", () => {
-  // Nav hamburger selections
-  const burger = document.querySelector("#burger-menu");
-  const ul = document.querySelector("nav ul");
-  const nav = document.querySelector("nav");
-
-  // Scroll to top selection
-  // const scrollUp = document.querySelector("#scroll-up");
-
-  // Select nav links
-  const navLink = document.querySelectorAll(".nav-link");
-  ul.classList.remove("show");
-
-  // Hamburger menu function
-  burger.addEventListener("click", () => {
-    // console.log("Clicked burger menu");
-    ul.classList.toggle("show");
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('burger-menu').addEventListener('click', function() {
+    var dropdown = document.getElementById('dropdown');
+    if (dropdown.style.display === 'block') {
+      dropdown.style.display = 'none';
+    } else {
+      dropdown.style.display = 'block';
+    }
   });
-
-
-
-  // Close hamburger menu when a link is clicked
-  // navLink.forEach((link) =>
-  //   link.addEventListener("click", () => {
-  //     ul.classList.remove("show");
-  //   })
-  // );
-
-  // Scroll to top functionality
-  // scrollUp.addEventListener("click", () => {
-  //   window.scrollTo({
-  //     top: 0,
-  //     left: 0,
-  //     behavior: "smooth",
-  //   });
-  // });
 });
